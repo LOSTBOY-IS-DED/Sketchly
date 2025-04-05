e9e7c00be918209ab304e7a80358ae64d3b1f625
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-04-06 04:08:02 +0530

    gated the ws server with the token

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,25 @@
 import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
+
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws , req) {
+
+  const url = req.url ;  //ws://localhost:3000?token=239482039
+  if(!url){
+    return ; 
+  }
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") as string;
+
+  const decoded = jwt.verify(token , JWT_SECRET);
+
+  if(!decoded || !(decoded as JwtPayload).userId){
+    ws.close();
+      return ; 
+  }
 
   ws.on('message', function message(data) {
       ws.send(`pong`);
