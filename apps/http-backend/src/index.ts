de11af0d26b44b2e1384e82e36a8e2c76c41a4f6
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-04-06 17:56:02 +0530

    added the zod schema to http layer

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -9,17 +9,39 @@ const app = express();
 app.use(express.json());
 
 app.post("/signin" , (req , res) => {
+    const data = SignInSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "incorrect inputs"
+        })
+        return ;
+    }
     res.json({
         message : "signed in"
     })
 })
 app.post("/signup" , (req , res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "incorrect inputs"
+        })
+        return ;
+    }
+
     const userId = 123 ; 
     const token = JWT.sign({ userId } , JWT_SECRET);
     res.json({ token });
     
 })
 app.post("/create-room" , authMiddleware , (req , res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "incorrect inputs"
+        })
+        return ;
+    }
     res.json({
         message : "created room"
     })
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import JWT from "jsonwebtoken";
-import { JWT_SECRET } from "./config.js";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 interface AuthRequest extends Request {
     userId?: string;
